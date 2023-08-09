5dedfd5735031e104fb9cf40c49ea6d693f2cd94
gsinoglu <137195153+gsinoglu@users.noreply.github.com>
2023-08-09 19:01:31 +0300

    HTML-CSS

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+// Don’t forget to `npm install sass`!
+const sass = require("sass");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addTemplateFormats("scss");
+
+  // Creates the extension for use
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css", // optional, default: "html"
+
+    // `compile` is called once per .scss file in the input directory
+    compile: async function(inputContent) {
+      let result = sass.compileString(inputContent);
+
+      // This is the render function, `data` is the full data cascade
+      return async (data) => {
+        return result.css;
+      };
+    }
+  });
+};
